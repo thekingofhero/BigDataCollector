d6610761e0dd99dee6c9ba5b42e4e2ec194aaf0e
thekingofhero <thekingofhero@aliyun.com>
2016-01-12 05:44:30 +0800

    collect into csv

--- environment_stats/collect_data/collect_data.py
+++ environment_stats/collect_data/collect_data.py
@@ -1,10 +1,51 @@
+import threading
+import datetime
+from getdata import getdata
+import math
 class collect_data:
     def __init__(self):
-        self.baseurl = "http://datacenter.mep.gov.cn/report/air_daily/airDairyCityHour.jsp?city=&startdate=%s%%20%s&enddate=%s%%20%s&page=%s"
+        self.baseurl = "http://datacenter.mep.gov.cn/report/air_daily/airDairyCityHour.jsp?city=&startdate=%s%%20%s&enddate=%s%%20%s&page=1"
         pass
+
+    def get_task_days(self,start_date,start_time,end_date,end_time):
+        task_days = []
+        start_t = datetime.datetime.strptime("%s %s"%(start_date,start_time),\
+                                            "%Y-%m-%d %H:%M")
+        end_t = datetime.datetime.strptime("%s %s"%(end_date,end_time),\
+                                            "%Y-%m-%d %H:%M")
+        days = (end_t - start_t).days
+        #total tasks,hours left
+        for i in range(1,days+2):
+            s = str(start_t).split(" ")
+            e = str(start_t + datetime.timedelta(i)).split(" ")
+            if (start_t + datetime.timedelta(i)) > end_t:
+                e = str(end_t).split(" ")
+            task_days.append((s,e))
+        return task_days
+
     def collect(self,thread_num = 1,\
                     start_date = '2014-01-01',\
                     start_time = "00:00",\
-                    end_date = '2014-01-02',\
-                    end_time = '00:00'):
-        pass
+                    end_date = '2014-01-01',\
+                    end_time = '01:00'):
+        thread_num = 10
+        total_tasks = self.get_task_days(start_date,start_time,end_date,end_time)
+        url_list = []
+        for s,e in total_tasks:
+            url = self.baseurl%(s[0],s[1],e[0],e[1])
+            url_list.append(url)
+        max_task_num_per_thread = int(math.ceil(len(url_list)/float(thread_num)))
+        temp = [url_list[i:i+max_task_num_per_thread] for i in range(0,len(url_list),max_task_num_per_thread)]
+        thread_list = []
+        for temp_i in temp:
+            print(temp_i)
+            t = threading.Thread(target=getdata,args=[temp_i])
+            thread_list.append(t)
+            t.start()
+        for t in thread_list:
+            if t.is_alive():
+                t.join()
+
+if __name__ == '__main__':
+    obj = collect_data()
+    obj.collect()
--- environment_stats/collect_data/getdata.py
+++ environment_stats/collect_data/getdata.py
@@ -1,42 +1,78 @@
+#-*- coding:utf-8 -*-
 try:
     from urllib.request import urlopen,Request
 except ImportError:
     from urllib2 import urlopen,Request
 from bs4 import BeautifulSoup
-    
-temp = ''
-#url = urlopen('http://datacenter.mep.gov.cn/report/air_daily/air_dairy.jsp?city=&startdate=2012-12-05&enddate=2015-12-22&page=1')
-url = urlopen('http://datacenter.mep.gov.cn/report/air_daily/airDairyCityHour.jsp?city=&startdate=2014-01-01%2008:00&enddate=2014-01-01%2011:00&page=2')
-#with open('./a.xml') as url:
-if 1:
-    temp = url.read()
-    temp = temp.replace("\n",'')
-    soup = BeautifulSoup(temp,"lxml")
+import re
+import time
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+def get_pages(soup,url):
+    #page number
+    page_rela = []
+    for b in soup.find_all("b"):
+        for child in b:
+            if b.string is None:
+                continue
+            page_rela.append(int(b.string))
+    total_rec,total_page,each_page = page_rela
+    pages = [''.join([url[:-1],str(i)]) for i in range(1,total_page+1)]
+    return pages
+
+def get_envdata(soup,file_p,head_flag):
+    #env data
     for tr in soup.find_all("tr"):
-    	if len(tr.attrs) == 2:
+        if len(tr.attrs) == 2:
             keys = tr.attrs.keys()
             keys.sort()
             if cmp(keys,['height', 'style']) == 0:
                 #other info
                 if tr['height'] == "25":
+                    for child in tr:
+                        if child.string is None:
+                            continue
                     pass
                 #data
                 elif tr['height'] == "30":
                     row = ''
+                    #delete "全 国 城 市 空 气 质 量 小 时 报"
+                    if len(tr) < 13:
+                        continue
                     for child in tr:
                         if child.string is None:
+                            child.string = 'None'
+                           # continue
+                        if child.string == ' ':
                             continue
                         if row == '':
                             row = ''.join([row,child.string])
                         else:
                             row = ''.join([row,',',child.string])
-                    print(row.strip(' ').strip(","))
-#                        for c in child.next_siblings:
-#                            try:
-#                                print(c.string)
-#                            except:
-#                                import sys
-#                                reload(sys)
-#                                sys.setdefaultencoding('utf8')
-#                                print(c.string)
-    #print (len(soup.find('tr')))
+                    #head needs only once
+                    if '序号' in row \
+                        and head_flag is False:
+                        continue
+                    print >> file_p ,row.strip(' ').strip(",")
+        #print (len(soup.find('tr')))
+
+def getdata(url_list):    
+    temp = ''
+    #url = urlopen('http://datacenter.mep.gov.cn/report/air_daily/air_dairy.jsp?city=&startdate=2012-12-05&enddate=2015-12-22&page=1')
+    #    url = urlopen('http://datacenter.mep.gov.cn/report/air_daily/airDairyCityHour.jsp?city=&startdate=2014-01-01%2008:00&enddate=2014-01-01%2011:00&page=2')
+    for url_per in url_list:
+        url = urlopen(url_per)
+        temp = url.read()
+        temp = temp.replace("\n",'')
+        soup = BeautifulSoup(temp,"lxml")
+#        with open(r'./data_xml/%s.xml'%(url_per.split(".jsp?")[1]),'w') as fpw:
+#            fpw.write(soup.prettify())
+        #get all pages
+        with open(r'./data_csv/%s.csv'%(url_per.split(".jsp?")[1][0:-7]),'w') as fpw:
+            pages = get_pages(soup,url_per)
+            for i,page in enumerate(pages):
+                soup = BeautifulSoup(urlopen(page).read().replace("\n",""))
+                get_envdata(soup,fpw,True if i == 0 else False)
+                time.sleep(5)
+
